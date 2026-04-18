1bd1411799151d1ee7fedf5b87bbfbc58cebf3af
Rafael Agustin López Hernández <donrafiki@Rafaels-Office-Mac-mini.local>
2026-04-18 13:35:47 -0600

    Mobile part is finished, but can appear other details

--- src/UI/App.tsx
+++ src/UI/App.tsx
@@ -1,22 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 import '../styles/CSS/App.css';
 import LogoLong from '../assets/Logo_Totalplay_long.svg';
 import Img_Gadgets from '../assets/Totalplay_gadgets.png';
+
 import plans_img from '../assets/Plans_complete.png';
+import plan_150 from '../assets/Plan_150mb.png';
+import plan_250 from '../assets/Plan_250mb.png';
+import plan_350 from '../assets/Plan_350mb.png';
+
 import award_img from '../assets/Award.png';
 import avatar from '../assets/user_icon.png';
 
 import awards_img from '../assets/Award_icons.png';
 import PROFECO from '../assets/PROFECO.png';
 import awardsAndProfeco from '../assets/Awards&PROFECO.png';
-import carousselImg from '../assets/Caroussel.png';
+import carouselAds from '../assets/carousselAds.png';
+import carouselPlans from '../assets/carousselPlans.png';
 
 export default function App() {
 
   return (
     <>
+    {/* HEADER, uses _header.scss for styles */}
       <div id='header'>
         {/* <div id='header-top-desktop' style={{display: 'none'}}>El internet mas premiado de México VER PAQUETES</div> */}
         <div id='header-top-mobile'>
@@ -33,9 +41,29 @@ export default function App() {
         </div>
         <div id='header-bottom-mobile' className='toolbar'>
           <div className='row align-items-center' id='threeItems'>
+            {/* HAMBURGER MENU START */}
             <div className='col' id='hamburgerMenu'>
-              <i className='bi bi-list'></i>
+              
+              <button className="btn btn-no" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTP" aria-controls="offcanvasExample">
+                <i className='bi bi-list'></i>
+              </button>
+
+              <div className="offcanvas offcanvas-start" id="offcanvasTP" aria-labelledby="offcanvasLabel">
+              <div className="offcanvas-header">
+                <h5 className="offcanvas-title" id="offcanvasLabel">Totalplay</h5>
+                <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+              </div>
+              <div className="offcanvas-body">
+                <ul id='offcanvas_options'>
+                  <li><a href="#">Nuestras ventajas</a></li>
+                  <li><a href="#">Paquetes</a></li>
+                  <li><a href="#">Soporte</a></li>
+                  <li><a href="#">Servicios</a></li>
+                </ul>
+              </div>
             </div>
+            </div>
+            {/* HAMBURGER MENU END */}
             <div className='col'>
               <img src={LogoLong} className='img-fluid'></img>
             </div>
@@ -43,27 +71,27 @@ export default function App() {
               <img src={avatar} className='img-fluid'></img>
             </div>
           </div>
-          <div className='row align-items-center'>
+          <div className='row align-items-center' id='dropdownAndImg'>
             {/* DROPDOWN START */}
-            <div className='dropdown col'>
-              <button type="button" className="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown button
+            <div className='dropdown col' id='dropdownCity'>
+              <button type="button" className="btn btn-dropdown dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                Ciudad de México<i className='bi bi-chevron-down'></i>
               </button>
               <ul className="dropdown-menu">
-                <li><a className="dropdown-item" href="#">Action</a></li>
-                <li><button className="dropdown-item" type="button">Another action</button></li>
-                <li><button className="dropdown-item" type="button">Something else here</button></li>
+                <li><a className="dropdown-item" href="#">Puebla</a></li>
+                <li><button className="dropdown-item" type="button">Estado de México</button></li>
+                <li><button className="dropdown-item" type="button">Monterrey</button></li>
               </ul>
             </div>
             {/* DROPDOWN END */}
             <div className='col'>
               <img src={PROFECO} className='img-fluid'></img>
             </div>
-            
           </div>
-          
         </div>
       </div>
+
+      {/* BODY, uses _content.scss for styles */}
       <div id='body'>
         <div id='body-section-ads'>
           <h1>No te pierdas ni un solo momento del Juego con el internet de Totalplay</h1>
@@ -71,21 +99,29 @@ export default function App() {
           <button className='btn btn-dark btn-lg rounded-pill'>Ver paquetes<i className='bi bi-chevron-right'></i></button>
           <p>Cuando el partido importa, la conexión también: cada jugada, sin interrupciones</p>
           <div>
-            <img src={carousselImg}></img>
+            <img src={carouselAds}></img>
           </div>
         </div>
         <div id='body-section-plans'>
           <h1>Nuestros planes más vendidos</h1>
-          <img src={plans_img} className='img-fluid'></img>
+          {/* <img src={plans_img} className='img-fluid'></img> */}
+          <div id='carousel'>
+            <img src={plan_150} className='img-fluid'></img>
+            <img src={plan_250} className='img-fluid'></img>
+            <img src={plan_350} className='img-fluid'></img>
+          </div>
+          <div>
+            <img src={carouselPlans}></img>
+          </div>
           <button className='btn btn-primary btn-lg rounded-pill'>Explorar más paquetes</button>
         </div>
         <div id='body-section-comments'>
           <h1>Más de 10 millones confían en nosotros</h1>
-          <div id='comments-names' className='overflow-visible'>
+          <div id='comments-names'>
             <button className='btn btn-dark rounded-pill'>Ángelica</button>
-            <button className='btn btn-dark rounded-pill'>Nery</button>
-            <button className='btn btn-dark rounded-pill'>Ivonne</button>
-            <button className='btn btn-dark rounded-pill'>Jose Jair</button>
+            <button className='btn btn-otherOption rounded-pill'>Nery</button>
+            <button className='btn btn-otherOption rounded-pill'>Ivonne</button>
+            <button className='btn btn-otherOption rounded-pill'>Jose Jair</button>
           </div>
           <div id='comments-stars'>
             <i className='bi bi-star-fill'></i>
@@ -104,7 +140,6 @@ export default function App() {
             <p>*Análisis realizado por Ookla ® de los datos de Speedtest intelligence ® para Q2-Q3 2024.</p>
           </div>
       </div>
-      {/* <div id='footer'></div> */}
     </>
   )
 }
